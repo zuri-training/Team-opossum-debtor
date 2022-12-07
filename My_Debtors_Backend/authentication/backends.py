e75264aa63abeac66c654cfde4a474e6a9f485f4
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-12-07 13:18:57 +0500

    configur user authentications and permissions logic

---
+++ My_Debtors_Backend/authentication/backends.py
@@ -0,0 +1,27 @@
+from rest_framework import authentication
+from django.contrib.auth.models import User
+from django.conf import settings
+import jwt
+
+
+class JWTAuthentication(authentication.BaseAuthentication):
+
+    def authenticate(self, request):
+        auth_data = authentication.get_authorization_header(request)
+
+        if not auth_data:
+            return None
+
+        prefix, token = auth_data.decode('utf-8').split(' ')
+
+        try:
+            payload = jwt.decode(token, settings.JWT_SECRET_KEY)
+            user = User.objects.get(username=payload['username'])
+            return (user, token)
+
+        except jwt.DecodeError as identifier:
+            raise exceptions.AuthenticationFailed('Invalid token')
+        except jwt.ExpiredSignatureError as identifier:
+            raise exceptions.AuthenticationFailed('Expired token')
+
+        return super().authenticate(request)
