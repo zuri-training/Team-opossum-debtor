55a8b037dca548efc9d70d05e54ab012b77f62a0
chimezdev <ochuba6@gmail.com>
2022-12-17 00:19:42 +0100

    added home url

--- My_Debtors_Backend/my_debtors_core/urls.py
+++ My_Debtors_Backend/my_debtors_core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include('my_debtors_api.urls')),
     path('auth/user/', include('authentication.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('my_debtors_api.urls'))
--- my_debtors/src/App.js
+++ my_debtors/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar";
 import Home from "./Pages/Home";
